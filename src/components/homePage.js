4bc06de88b09dbd86d8cc08ea722080ef85964d8
Gabriel Graeff <gabriel_graeff@hotmail.com>
2016-08-25 15:57:14 -0300

    Config project and start create a component in JSX

---
+++ src/components/homePage.js
@@ -0,0 +1,17 @@
+/**
+ * Created by graeff on 25/08/16.
+ */
+"use strict";
+
+var react = require('react');
+
+var Home = React.createClass({
+   render: function () {
+       return (
+           <div className="jumbotron">
+               <h1>Administração</h1>
+               <p>React for web</p>
+           </div>
+       );
+   }
+});
